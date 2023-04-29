093b345c62b4eb1850eebbc2317a6b1da7d61abb
Nguyễn Thanh Xuân <thanhxuanhd007@gmail.com>
2023-04-29 22:19:00 +0700

    Update nuget package to latest version

--- TodoApplication/Todo.Application/ApplicationServiceRegistration.cs
+++ TodoApplication/Todo.Application/ApplicationServiceRegistration.cs
@@ -1,5 +1,4 @@
-﻿using MediatR;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
@@ -11,7 +10,11 @@ public static class ApplicationServiceRegistration
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
-        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
 
         return services;
     }
